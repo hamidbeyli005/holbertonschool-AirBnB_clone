46ae8b7a76dc69c5f41ab208f26b043da876d3d0
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-02-29 12:55:26 +0000

    Updated test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -32,26 +32,17 @@ class TestFileStorage(unittest.TestCase):
 
         self.assertTrue(os.path.exists(FileStorage._FileStorage__file_path))
 
-    def test_reload(self):
-        self.storage.save()
-        file_path = "file.json"
+    def test_reload(self):    
+        if os.path.exists(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
 
-        with open(file_path, 'r') as f:
-            lines = f.readlines()
-
-        os.remove(file_path)
-        self.storage.save()
-
-        with open(file_path, 'r') as f:
-            lines2 = f.readlines()
-
-        self.assertEqual(lines, lines2)
-
-        with open(file_path, "w") as f:
-            f.write("{}")
-
-        with open(file_path, "r") as r:
-            self.assertEqual(r.read(), "{}")
-
-        self.assertIs(self.storage.reload(), None)
-        os.remove(file_path)
+        self.model.save()
+        u_id = self.model.id
+        actual = 0
+        new_storage = FileStorage()
+        new_storage.reload()
+        all_obj = new_storage.all()
+        for k in all_obj.keys():
+            if u_id in k:
+                actual = 1
+        self.assertTrue(0 == actual)
