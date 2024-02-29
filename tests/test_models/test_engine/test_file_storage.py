fff4730c5e3293a1fba363e0f8179e069fcd3302
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-02-29 13:42:04 +0000

    Updated test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -31,18 +31,13 @@ class TestFileStorage(unittest.TestCase):
         self.storage.save()
 
         self.assertTrue(os.path.exists(FileStorage._FileStorage__file_path))
-
-    def test_reload(self):    
-        if os.path.exists(FileStorage._FileStorage__file_path):
-            os.remove(FileStorage._FileStorage__file_path)
-
-        self.model.save()
-        u_id = self.model.id
-        actual = 0
-        new_storage = FileStorage()
-        new_storage.reload()
-        all_obj = new_storage.all()
-        for k in all_obj.keys():
-            if u_id in k:
-                actual = 1
-        self.assertTrue(0 == actual)
+    
+    def test_reload(self):
+        """Test case for 'reload' method"""
+        model = BaseModel()
+        self.storage.new(model)
+        self.storage.save()
+        self.storage._FileStorage__objects.clear()
+        self.storage.reload()
+        key = model.__class__.__name__ + "." + model.id
+        self.assertIn(key, self.storage.all()) 
